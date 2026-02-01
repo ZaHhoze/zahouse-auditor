88b24f60eb39101fd729d8f6838aaf35ccb91598
Darrel Crutchfield <darrelcrutchfield@Darrels-MacBook-Pro.local>
2026-02-01 13:30:39 -0500

    fix: restored correct CORS security headers

--- index.js
+++ index.js
@@ -109,4 +109,5 @@ app.post('/audit', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Forensic Server live on port ${PORT}`));
+app.listen(PORT, () => console.log(`Forensic Server live on port ${PORT}`);
+});
